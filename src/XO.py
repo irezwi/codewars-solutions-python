14fc8ca991c7e8a1c08e0ea536c661b990449218
irezwi <zwirek.ireneusz@gmail.com>
2018-02-19 14:10:09 +0100

    Refactored 'Exes and Ohs' Kata

--- src/XO.py
+++ src/XO.py
@@ -3,16 +3,4 @@
 
 
 def xo(s):
-    s = s.lower()
-    x = 0
-    o = 0
-
-    for letter in s:
-        if letter == 'x':
-            x += 1
-        elif letter == 'o':
-            o += 1
-    if x == o:
-        return True
-    else:
-        return False
+    return s.lower().count('x') == s.lower().count('o')
