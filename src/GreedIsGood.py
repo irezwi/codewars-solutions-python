b73aa1c7a277929a00ad11a2418021db59d03d7c
irezwi <zwirek.ireneusz@gmail.com>
2018-11-16 00:42:58 +0100

    "Greed is good" Kata refactored

--- src/GreedIsGood.py
+++ src/GreedIsGood.py
@@ -1,17 +1,10 @@
 def score(dice):
-    points = 0
-    d = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
-    for number in dice:
-        d[number] += 1
-    if d[1] >= 3:
-        points += 1000
-        d[1] -= 3
-    for number in range(2, 7):
-        if d[number] >= 3:
-            points += 100 * number
-            d[number] -= 3
-    if d[5] != 0:
-        points += 50 * d[5]
-    if d[1] != 0:
-        points += 100 * d[1]
-    return points
+    triplets_points = [1000, 200, 300, 400, 500, 600]
+    single_points = [100, 0, 0, 0, 50, 0]
+    score = 0
+
+    for number in range(1, 7):
+        if dice.count(number) >= 3:
+            score += triplets_points[number - 1]
+        score += single_points[number - 1] * (dice.count(number) % 3)
+    return score
