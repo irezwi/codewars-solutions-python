e615ca0839d6dfec695ce822c46464407b094190
irezwi <zwirek.ireneusz@gmail.com>
2020-10-29 22:01:33 +0100

    Rewrite old solutions

--- src/AreTheyTheSame.py
+++ src/AreTheyTheSame.py
@@ -2,14 +2,7 @@
 # https://www.codewars.com/kata/550498447451fbbd7600041c
 
 
-import math
-
-
 def comp(array1, array2):
     if array1 is None or array2 is None:
         return False
-    else:
-        for i in range(0, len(array2)):
-            if math.sqrt(array2[i]) not in array1:
-                return False
-        return True
+    return sorted(list(map(lambda x: x ** 2, array1))) == sorted(array2)
