79f126a26e9202b3c9c3d7e1a551f0ead0c4dfa3
irezwi <zwirek.ireneusz@gmail.com>
2020-10-31 15:45:08 +0100

    "Memoized Fibonacci" Kata solved

---
+++ src/MemoizedFibonacci.py
@@ -0,0 +1,21 @@
+# Memoized Fibonacci
+# https://www.codewars.com/kata/529adbf7533b761c560004e5
+
+
+def cache(func):
+    cache_dict = dict()
+
+    def func_wrapper(*args, **kwargs):
+        n = args[0]
+        if n not in cache_dict:
+            cache_dict[n] = func(*args, **kwargs)
+        return cache_dict[n]
+
+    return func_wrapper
+
+
+@cache
+def fibonacci(n):
+    if n in [0, 1]:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
